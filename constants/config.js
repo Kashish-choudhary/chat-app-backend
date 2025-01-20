1aa140fa8f813c69448d5c349953fc32cf666b64
Kashish-choudhary <kashishchoudharykj@gmail.com>
2025-01-20 12:14:23 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -1,9 +1,5 @@
 const corsOptions = {
-  origin: [
-    "http://localhost:5174",
-    "http://localhost:4173",
-    process.env.CLIENT_URL,
-  ],
+  origin: process.env.CLIENT_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
