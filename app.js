6de3079bc6318a54969b5fcf24b80a1107fd654c
Kashish-choudhary <kashishchoudharykj@gmail.com>
2025-01-20 13:20:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.set("io", io);
 // Using Middlewares Here
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/chat", chatRoute);
